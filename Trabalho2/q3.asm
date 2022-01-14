f50ed6485cb36185ab487f9727025b1695c02c23
Markson Arguello <57077854+MarksonArguello@users.noreply.github.com>
2022-01-14 12:02:04 -0300

    Imprimindo "overflow" no banner

--- Trabalho2/q3.asm
+++ Trabalho2/q3.asm
@@ -9,109 +9,118 @@ DISPLAY EQU 0
 BANNER  EQU 2
 CLEARBANNER EQU 3
 
-ORG 400
-    PRODINT: DB 0  ; Resultado
 
-ORG 500
-    TAM: DB 3 ; Tamanho dos vetores
-    U: DB 37, 42, 66 ; Vetor U
-    V: DB 10, 10, 10 ; Vetor V
 
-    PTRU: DW U ;Ponteiro para U
-    PTRV: DW V ; Ponteiro para V
+ORG 1100
+                 TAM: DB 6 ; Tamanho dos vetores
+                 U: DB 127, 127, 127, 127, 127, 127 ; Vetor U
+                 V: DB 100, 100, 100, 100, 100, 100; Vetor V
 
-    ; Índices
-    I: DB 0
-    J: DB 0
+                 PTRU: DW U ;Ponteiro para U
+                 PTRV: DW V ; Ponteiro para V
 
-    ;Caracter a ser escrito no banner
-    CARACTER: DB 0
+                 ; Índices
+                 I: DB 0
+                 J: DB 0
+
+                 ;Caracter a ser escrito no banner
+                 CARACTER: DB 0
+
+                 ;String a ser imprimida no banner em caso de overflow
+
+
+
+ORG 1000
+                 PRODINT: DB 0  ; Resultado
 
 ORG 0
 
 
 INIT:
-     OUT CLEARBANNER ;Limpa o Banner
+                 OUT CLEARBANNER ;Limpa o Banner
 
 LOOP:
-     LDA I ; ACC = I
-     SUB TAM ; ACC = ACC - TAM  = I - TAM
-     JZ PRINTAR ; CASO I < TAM VAI PRA MULTIPLICACAO E CASO I == TAM PRINTA O RESULTADO
+                 LDA I ; ACC = I
+                 SUB TAM ; ACC = ACC - TAM  = I - TAM
+                 JZ PRINTAR ; CASO I < TAM VAI PRA MULTIPLICACAO E CASO I == TAM PRINTA O RESULTADO
 
 MULTIPLICACAO:
-              LDA J ; ACC = J
-              SUB @PTRV ; J - @PTRV
-              JZ INCR ; CASO I < @PTRV CONTINUA SOMANDO, CASO I == @PTRV SAI DA MULTIPLICACAO
+                LDA J ; ACC = J
+                SUB @PTRV ; J - @PTRV
+                JZ INCR ; CASO I < @PTRV CONTINUA SOMANDO, CASO I == @PTRV SAI DA MULTIPLICACAO
 
-              ;J++
-              LDA J
-              ADD #1
-              STA J
+                ;J++
+                LDA J
+                ADD #1
+                STA J
 
-              ; PRODINT += @PTRU
-              LDA PRODINT
-              ADD @PTRU
-              STA PRODINT
+                ; PRODINT += @PTRU
+                LDA PRODINT
+                ADD @PTRU
+                STA PRODINT
 
-              ; ADICIONO CASO TENHA DADO OVERFLOW EM 8 BITS
-              LDA PRODINT+1
-              ADC #0
-              STA PRODINT+1
+                ; ADICIONO CASO TENHA DADO OVERFLOW EM 8 BITS
+                LDA PRODINT+1
+                ADC #0
+                JC OVERFLOW ; IMPRIMO OVERFLOW NO BANNER
+                STA PRODINT+1
 
-              ; VOLTO PARA O COMEÇO
-              JMP MULTIPLICACAO
+
+
+                ; VOLTO PARA O COMEÇO
+                JMP MULTIPLICACAO
 
 INCR:
-     ; I++
-     LDA I
-     ADD #1
-     STA I
+                ; I++
+                LDA I
+                ADD #1
+                STA I
 
-     ; J = 0
-     LDA #0
-     STA J
+                ; J = 0
+                LDA #0
+                STA J
 
-     ; PTRU++
-     LDA PTRU
-     ADD #1
-     STA PTRU
+                ; PTRU++
+                LDA PTRU
+                ADD #1
+                STA PTRU
 
-     ;PTRV++
-     LDA PTRV
-     ADD #1
-     STA PTRV
+                ;PTRV++
+                LDA PTRV
+                ADD #1
+                STA PTRV
 
-     ; VOLTO PARA O COMEÇO DO LOOP
-     JMP LOOP
+                ; VOLTO PARA O COMEÇO DO LOOP
+                JMP LOOP
 
 
-PRINTAR: ; A PARTIR DAQUI SERVE PARA PRINTAR O RESULTADO
+PRINTAR:         ; A PARTIR DAQUI SERVE PARA PRINTAR O RESULTADO
 
 PRIMEIRO_NUMERO: ; O PRIMEIRO NÚMERO SÃO OS 4 PRIMEIROS BITS DO NÚMERO DE 16 BITS
 
-              ; PEGO APENAS OS 4 BITS MAIS SIGNIFICATIVOS E GUARDO EM CARACTER
-              LDA PRODINT+1
-              SHR
-              SHR
-              SHR
-              SHR
-              STA CARACTER
+                 ; PEGO APENAS OS 4 BITS MAIS SIGNIFICATIVOS E GUARDO EM CARACTER
+               LDA PRODINT+1
+               SHR
+               SHR
+               SHR
+               SHR
+               STA CARACTER
 
-              ; TESTO SE O NÚMERO É MENOR QUE AH OU 10 DECIMAL
-              SUB #10
-              JN  DEC0 ; CASO SEJA MENOR QUE 10 PULO PARA ONDE CONVERTE DE 0 A 9 PARA ASCII
-              ; CASO SEJA MAIOR CONINUO PARA CONVERTER DE LETRA PARA ASCII
+               ; TESTO SE O NÚMERO É MENOR QUE AH OU 10 DECIMAL
+               SUB #10
+               JN  DEC0 ; CASO SEJA MENOR QUE 10 PULO PARA ONDE CONVERTE DE 0 A 9 PARA ASCII
+               ; CASO SEJA MAIOR CONINUO PARA CONVERTER DE LETRA PARA ASCII
 
-              LDA CARACTER ; CARREGO O CARACTER
-              ADD #37H ; CONVERTO HEXADECIMAL MAIOR QUE 9 PARA ASCII
-              OUT BANNER ; ESCREVO NO BANNER
+               LDA CARACTER ; CARREGO O CARACTER
+               ADD #37H ; CONVERTO HEXADECIMAL MAIOR QUE 9 PARA ASCII
+               OUT BANNER ; ESCREVO NO BANNER
 
-              JMP SEGUNDO_NUMERO ; VOU PARA O PRÓXIMO NÚMERO
+               JMP SEGUNDO_NUMERO ; VOU PARA O PRÓXIMO NÚMERO
 
-          DEC0: ; CONVERTE NÚMEROS DECIMAIS PARA ASCII
-              LDA CARACTER
-              ADD #30H
-              OUT BANNER
+DEC0: ; CONVERTE NÚMEROS DECIMAIS PARA ASCII
+               LDA CARACTER
+               ADD #30H
+               OUT BANNER
 
 
 
@@ -136,7 +145,7 @@ SEGUNDO_NUMERO:
                 OUT BANNER
                 JMP TERCEIRO_NUMERO
 
-          DEC1:
+DEC1:
                LDA CARACTER
                ADD #30H
                OUT BANNER
@@ -146,58 +155,76 @@ SEGUNDO_NUMERO:
 
 
 TERCEIRO_NUMERO:
-    LDA PRODINT
-    SHR
-    SHR
-    SHR
-    SHR
-    STA CARACTER
+                LDA PRODINT
+                SHR
+                SHR
+                SHR
+                SHR
+                STA CARACTER
 
-    SUB #10
-    JN  DEC2
+                SUB #10
+                JN  DEC2
 
-    LDA CARACTER
-    ADD #37H
-    OUT BANNER
+                LDA CARACTER
+                ADD #37H
+                OUT BANNER
 
-    JMP QUARTO_NUMERO
+                JMP QUARTO_NUMERO
 
 DEC2:
-    LDA CARACTER
-    ADD #30H
-    OUT BANNER
+                LDA CARACTER
+                ADD #30H
+                OUT BANNER
 
 
 
 
 
 QUARTO_NUMERO:
-      LDA CARACTER
-      SHL
-      SHL
-      SHL
-      SHL
-      STA CARACTER
+              LDA CARACTER
+              SHL
+              SHL
+              SHL
+              SHL
+              STA CARACTER
 
 
-      LDA PRODINT
-      SUB CARACTER
-      STA CARACTER
+              LDA PRODINT
+              SUB CARACTER
+              STA CARACTER
 
-      SUB #10
-      JN  DEC3
+              SUB #10
+              JN  DEC3
 
-      LDA CARACTER
-      ADD #37H
-      OUT BANNER
+              LDA CARACTER
+              ADD #37H
+              OUT BANNER
 
-      JMP FIM
+              JMP FIM
 
 DEC3:
-     LDA CARACTER
-     ADD #30H
-     OUT BANNER
+              LDA CARACTER
+              ADD #30H
+              OUT BANNER
+              JMP FIM
 
+OVERFLOW:
+             ADC #0
+             LDA @PTRS ; Le um caracter
+             OR #0 ; Se for NULL
+             JZ FIM ; Termina
+             OUT BANNER ;Senão escreve no banner
 
-FIM:    HLT
+             ;PTRS++
+             LDA PTRS
+             ADD #1
+             STA PTRS
+
+             JMP OVERFLOW
+
+FIM:         HLT
+
+OVER:        STR    "Overflow"
+             DB     0                    ; Termina com NULL
+PTRS:        DW     OVER
 
